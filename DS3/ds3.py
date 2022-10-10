b088f9197a538e2fa97e179c9eec67c6f611d641
charanhu <charanhumail@gmail.com>
2022-10-10 16:06:13 +0530

    ds3

--- DS3/ds3.py
+++ DS3/ds3.py
@@ -1,8 +1,12 @@
-# dict.get
+# python dictioanry get() method
+# defination: The get() method returns the value of the item with the specified key if the key exists. 
+# If the key does not exist, it returns the specified default value if specified, otherwise None.
 
 locations = {'meeting1':'room1', 'meeting2':'room2'}
 
 print(locations.get('meeting1', 'online'))
+# Output: room1
 
-# to get the key that is not in the dict
+# to get the default value if the key is not found
 print(locations.get('meeting3', 'online'))
+# Output: online
